f4463d8879c34b163b6e3d6795c8e9e6e9228b62
Çağlar ERGÜL <caglarergul@me.com>
2018-01-04 19:11:27 +0300

    Router and Navigation is created.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 //import axios from 'axios';
+import {Route, Link} from 'react-router-dom';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 
 
@@ -19,13 +21,18 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname:'/new-post'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
 
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />*/}
+
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post"  component={NewPost} />
 
             </div>
         );
